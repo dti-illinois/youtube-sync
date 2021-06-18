cbddf5e4852173269b0f28ec173e168378c0f83b
Nicholas Anand <nicholas@nmoleosoftware.com>
2021-06-18 11:39:16 -0500

    Created new enviornment for redesign

--- app.py
+++ app.py
@@ -73,26 +73,7 @@ login_manager.init_app(app)
 sio = SocketIO(app, cors_allowed_origins='*')
 # endregion
 
-# region Define variables
-# Stores a dictionary of information about each user - their username and their role as a host or a guest
-users = {}
-
-# The SID of the host user
-host_sid = ""
-
-# Stores the chat history to send to a new user when they join
-chat_history = []
-
-# The URL of the YouTube video
-url = ""
-
-changing_host = False
-
-HOST_ROLE = 0
-GUEST_ROLE = 1
-# endregion
-
-
+# region Shibboleth
 @login_manager.user_loader
 def load_user(netid):
     return User.get(netid)
@@ -139,19 +120,7 @@ def callback():
     login_user(user)
 
     return redirect(url_for("index"))
-
-
-# Resets all data to the original state
-def Reset():
-    global users
-    global host_sid
-    global chat_history
-    global url
-
-    users = {}
-    host_sid = ""
-    chat_history = []
-    url = ""
+# endregion
 
 
 @app.route('/')
@@ -159,7 +128,7 @@ def index():
     if current_user.is_authenticated:
         user = current_user.id
         log("Sending rendered page '/'", request)
-        return render_template("video-join-page.html")
+        return render_template("index.html")
     else:
         return redirect(url_for("login"))
  
@@ -171,192 +140,9 @@ def logout():
     return redirect(app.config["ISSUER_URL"] + "/idp/profile/Logout")
 
 
-@app.route('/video-join-page')
-def VideoJoinPage():
-    log("Sending rendered page '/video-join-page'", request)
-    return render_template("video-join-page.html")
-
-
-@app.route('/video-player')
-def VideoPlayer():
-    log("Sending rendered page '/video-player'", request)
-    return render_template("video-player.html")
-
-
-# This is called when the client pings the server to find out if there is already a host
-# If there isn't one, the client will auto-select the "Host" button
-@app.route('/current-host-check')
-def CurrentHostCheck():
-    log("Sending non-rendered page '/current-host-check'", request)
-    host_exists = False
-    for sid in users:
-        if users[sid]["role"] == HOST_ROLE:
-            host_exists = True
-    if host_exists:
-        return "true"
-    else:
-        return "false"
-
-
-# Takes a webRequest object and checks if the SID of the sender matches the SID of the session host
-# If yes, returns true. If no, returns false and makes a report of the infraction in the log file
-def CheckIfHost(webRequest, message):
-    if (webRequest.sid == host_sid):
-        return True
-    else:
-        log("Received the following request: " + json.dumps(message) + "\n\tDenying request because user falsely claimed to be the host.", request)
-        return False
-
-
-# region Websockets Message Handler
-@sio.on('message')
-def HandleMessage(message):
-    global users
-    global host_sid
-    global chat_history
-    global url
-    global changing_host
-
-    # region Guest Join Requests
-    if message["type"] == "join" and message["role"] == GUEST_ROLE:
-        print("Received guest request...")
-
-        if (host_sid == "" and not changing_host):
-            send({"type": "guest_request_response", "value": False, "reason": "no_host"})
-            log("Received join request with requested username '" + message["name"] + "'. Denied for reason: there is not a host in this session", request)
-
-        # Validate username
-        else:
-            usernameValidation = ValidateUsername(message["name"], GUEST_ROLE, users, changing_host, request)
-
-            if (usernameValidation["value"] == True):
-                # Alert the user of the success
-                send({"type": "guest_request_response", "value": True})
-
-                # Send the user the previous chat history
-                send({"type": "chat_history", "data": chat_history})
-
-                # Send the user the video URL
-                send({"type": "change_video_url", "url": url})
-
-                # Alert the host that a new guest has joined - the host will send the current video state to the guest
-                send({"type": "guest_joined", "name": message["name"]}, broadcast=True)
-
-                # Set user data for this new guest
-                users[request.sid] = {"role": GUEST_ROLE, "username": message["name"]}
-
-                # Send the current user data to all clients
-                send({"type": "user_data", "data": users}, broadcast=True)
-            else:
-                send({"type": "guest_request_response", "value": False, "reason": usernameValidation["reason"]})
-    # endregion Join Requests
-
-    # region Host Join Requests
-    elif message["type"] == "join" and message["role"] == HOST_ROLE:
-        # Check if there is already an existing host user
-        if (host_sid != ""):
-            send({"type": "host_request_response", "value": False, "reason": "host_already_exists"})
-            log("Received host request with requested username '" + message["name"] + "'. Denied for reason: there is already a host in this session", request)
-        # Validate username
-        else:
-            usernameValidation = ValidateUsername(message["name"], HOST_ROLE, users, changing_host, request)
-            if (usernameValidation["value"] == True):
-                # Alert the user of the success
-                send({"type": "host_request_response", "value": True})
-
-                # Save the host's SID
-                host_sid = request.sid
-
-                # Set user data for this new host
-                users[request.sid] = {"role": HOST_ROLE, "username": message["name"]}
-
-                # Save the video URL
-                url = message["url"]
-
-                # Send the host the current user data
-                send({"type": "user_data", "data": users}, broadcast=True)
-
-                # Send the host the video URL
-                send({"type": "change_video_url", "url": url}, broadcast=True)
-
-                # End the 'changing host' state (if applicable)
-                changing_host = False
-            else:
-                send({"type": "host_request_response", "value": False, "reason": usernameValidation["reason"]})
-    # endregion
-
-    # region Host Video Data
-    elif message["type"] == "host_data":
-        if (CheckIfHost(request, message)):
-            send({"type": "player_data", "data": message["data"]}, broadcast=True)
-            log("Received host video data.", request)
-    # endregion
-
-    # region Guest Video Data
-    elif message["type"] == "guest_data":
-        send({"type": "guest_data", "action": message["action"], "timestamp": message["timestamp"]}, broadcast=True)
-        log("Received guest video data.", request)
-    # endregion
-
-    # region User Kick Requests
-    elif message["type"] == "kick_user":
-        if (CheckIfHost(request, message)):
-            send({"type": "kick_user", "user": message["user"]}, broadcast=True)
-
-            kick_sid = ""
-            for sid, user in users.items():
-                if (user["username"] == message["user"]):
-                    kick_sid = sid
-
-            if (kick_sid != ""):
-                disconnect(sid)
-
-            log("The host kicked user: " + message["user"], request)
-    # endregion
-
-    # region Promotion Requests
-    elif message["type"] == "promote_user":
-        if (CheckIfHost(request, message)):
-            changing_host = True
-
-            send({"type": "promote_user", "user": message["user"], "video_state": message["video_state"]}, broadcast=True)
-            log("Promoted user " + message["user"] + " to host", request)
-
-            del users[host_sid]
-            host_sid = ""
-
-            send({"type": "user_data", "data": users}, broadcast=True)
-    # endregion
-
-    # region Changing Video URL
-    elif message["type"] == "change_video_url":
-        if (CheckIfHost(request, message)):
-            url = message["url"]
-            send({"type": "change_video_url", "url": url}, broadcast=True)
-            log("Changed video URL to " + message["url"], request)
-    # endregion
-
-    # region Chat Messages
-    elif message["type"] == "chat":
-        chat_history.append(message)
-        send(message, broadcast=True)
-        log("Received chat message: " + message["message"], request)
-    # endregion
-
-    # region Chat Message Removal
-    elif message["type"] == "remove_chat_message":
-        if (CheckIfHost(request, message)):
-            send({"type": "remove_chat_message", "message_index": message["message_index"]}, broadcast=True)
-            chat_history.pop(message["message_index"])
-            log("Host removed chat message: " + message["message_content"], request)
-    # endregion
-# endregion
-
-
 @sio.on('connect')
 def WebSocketsConnect():
     log("WebSockets client connected.", request)
-    users[request.sid] = {"username": "", "role": ""}
     send({"type": "connection_status", "value": True})
 
 
@@ -364,19 +150,8 @@ def WebSocketsConnect():
 def WebSocketsDisconnect():
     log("WebSockets client disconnected.", request)
 
-    if users[request.sid]["role"] == HOST_ROLE:
-        if not changing_host:
-            send({"type": "host_left"}, broadcast=True)
-            log("The host left the session", request)
-            Reset()
-    else:
-        log("Guest left the session", request)
-        del users[request.sid]
-        send({"type": "user_data", "data": users}, broadcast=True)
-
 
 # Run app
 if __name__ == '__main__':
-    # sio.run(app, host='play.dti.illinois.edu', port=443)
     log("Initializing app...")
-    sio.run(app, debug=True)
+    sio.run(app, debug=True, use_reloader=False)
